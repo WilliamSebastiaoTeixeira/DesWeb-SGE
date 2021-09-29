1a5b0381a6f594b404578a81c97b363d7c8450b9
William Sebastiao Teixeira <williansebastiao99@gmail.com>
2021-09-29 03:05:38 -0300

    visual

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,8 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/mapa'); 
+    //return view('dashboard');
 })->name('dashboard');
 
 Route::group(['middleware' => 'auth'], function () {
